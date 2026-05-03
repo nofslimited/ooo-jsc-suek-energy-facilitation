e2e322e31bcd4bfa01294562d8b813e1638e7be6
ngwache41 <ngwache41@gmail.com>
2026-05-03 12:15:25 +0300

    final fix language provider at root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,6 @@
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { LanguageProvider } from "./components/context/LanguageContext";
 
 export default function App() {
-  return (
-    <LanguageProvider>
-      <RouterProvider router={router} />
-    </LanguageProvider>
-  );
+  return <RouterProvider router={router} />;
 }
