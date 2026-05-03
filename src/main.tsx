ab4282f18583db09963b67c36b4596d3495727f7
ngwache41 <ngwache41@gmail.com>
2026-05-03 13:37:18 +0300

    fix app root provider and router structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
 import { LanguageProvider } from "./app/context/LanguageContext";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
