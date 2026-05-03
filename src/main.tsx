7d2635be3097f29cc814c48af8b147828c5e1688
ngwache41 <ngwache41@gmail.com>
2026-05-03 11:53:48 +0300

    fix global language provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import { LanguageProvider } from "./app/components/context/LanguageContext";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  </React.StrictMode>
+);
