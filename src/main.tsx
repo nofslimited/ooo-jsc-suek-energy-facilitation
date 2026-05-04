31f70b01e2227eb699f0d0c16bc812c42b9a135d
ngwache41 <ngwache41@gmail.com>
2026-05-04 16:48:48 +0300

    fix global css import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* =========================
-   GLOBAL STYLES (IMPORTANT)
-========================= */
-import "../styles/globals.css";
+import "./styles/globals.css";
 
-/* =========================
-   APP + CONTEXT
-========================= */
 import { LanguageProvider } from "./app/context/LanguageContext";
 import App from "./app/App";
 
-/* =========================
-   RENDER APP
-========================= */
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LanguageProvider>
